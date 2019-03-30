ca49dc83983cc9abfc8bc109b9df2b3b293c6371
Kgor <kevin.gorham@gmail.com>
2019-03-29 21:50:23 -0700

    save

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -18,9 +18,7 @@ const friendsData = require("../data/friends");
 module.exports = (app) => {
   // API GET Requests
   // Below code handles when users "visit" a page.
-  // In each of the below cases when a user visits a link
-  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
-  // ---------------------------------------------------------------------------
+  //---------------------------------------------------------------------------
 
   app.get("/api/friends", (req, res) => {
     res.json(friendsData);
