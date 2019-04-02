fab9bae6a4867efa097f380f876e8db76cdc8944
Kgor <kevin.gorham@gmail.com>
2019-04-01 22:57:41 -0700

    changed back

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -16,12 +16,12 @@ module.exports = (app) => {
   // In each of the below cases the user is shown an HTML page of content
   // ---------------------------------------------------------------------------
 
-  app.get("*", (req, res) => {
+  app.get("/survey", (req, res) => {
     res.sendFile(path.join(__dirname, "/../public/survey.html/"));
   });
 
   // If no matching route is found default to home
-  // app.get("*", (req, res) => {
-  //   res.sendFile(path.join(__dirname, "/../public/home.html"));
-  // });
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "/../public/home.html"));
+  });
 };
